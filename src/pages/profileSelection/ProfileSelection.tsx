b94b469f143e021be0eac1e1869f1e7d3dfd52d2
Joan Sanchis <jsalvadorsb@gmail.com>
2025-05-28 18:58:03 +0200

    resolved conflict in profile selection page

--- src/pages/profileSelection/ProfileSelection.tsx
+++ src/pages/profileSelection/ProfileSelection.tsx
@@ -1,5 +1,5 @@
 import { ProfileCard } from "../../components/profileCard/ProfileCard";
-import { getUserProfiles } from "../../dataBase/services/readFunctions";
+import { getProfilesFromUser } from "../../dataBase/services/readFunctions";
 import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../../auth/AuthContext";
 import { AddDogButton } from "../../components/addDogButton/AddDogButton";
@@ -16,7 +16,7 @@ export const ProfileSelection = () => {
     if (!user) return;
 
     const fetchUserProfiles = async () => {
-      const profilesQuery: ProfileData[] | null = await getUserProfiles(
+      const profilesQuery: ProfileData[] | null = await getProfilesFromUser(
         user.uid
       );
 
@@ -35,16 +35,16 @@ export const ProfileSelection = () => {
   if (!userProfiles) return;
 
   return (
-    <div className='profile-selection'>
-      <div className='profile-selection__info'>
-        <p className='profile-selection__title'>Select a profile</p>
-        <p className='profile-selection__text'>
+    <div className="profile-selection">
+      <div className="profile-selection__info">
+        <p className="profile-selection__title">Select a profile</p>
+        <p className="profile-selection__text">
           Manage and select profiles for your dogs. Each one lets you join
           events, connect with others, and track their activity. Got a new dog?
           Just create a profile.
         </p>
       </div>
-      <div className='profile-selection__profiles-container'>
+      <div className="profile-selection__profiles-container">
         {userProfiles.map((profile, index) => (
           <ProfileCard key={index} eventId={profile.id} />
         ))}
