fa29618ca7d20abdc6391d5fe24192e4fca65ec5
Alvaro Giner <aginer079j@gmail.com>
2025-05-29 08:30:56 +0200

    implemented db delete profile and event functions

---
+++ src/dataBase/services/deleteFunctions.ts
@@ -0,0 +1,24 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../dataBase/firebase";
+import { toast } from "react-toastify";
+
+// -----> Delete an Event
+export const deleteOneEvent = async (eventId: string) => {
+  try {
+    await deleteDoc(doc(db, "events", eventId));
+    toast("Your event was succesfully deleted");
+  } catch {
+    console.error(`Event with ID:${eventId} was not deleted`);
+  }
+};
+
+// -----> Delete a Profile
+//Comprobar si el id del pefil que eliminamos es el mismo que el del perfil loggeado, entonces, debemos decir que no puede hacerlo
+export const deleteOneProfile = async (profileId: string) => {
+  try {
+    await deleteDoc(doc(db, "profiles", profileId));
+    toast("Your profile was succesfully deleted");
+  } catch {
+    console.error(`Profile with ID:${profileId} was not deleted`);
+  }
+};
