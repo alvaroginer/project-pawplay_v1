00163c183ffbf54dd2c5ee92857e595880fb4e34
alvaroginer <aginer079j@gmail.com>
2025-06-09 16:39:53 +0200

    working on formfieldlocation functionality

--- src/components/formField/FormFieldLocation.tsx
+++ src/components/formField/FormFieldLocation.tsx
@@ -1,66 +1,64 @@
 import { AddressAutofill } from "@mapbox/search-js-react";
-import { useFormContext, useController } from "react-hook-form";
-import { GeoPoint } from "firebase/firestore";
+import { FieldErrors, Path, UseFormRegister } from "react-hook-form";
+
+// import { useFormContext, useController } from "react-hook-form";
+// import { GeoPoint } from "firebase/firestore";
 import locationIcon from "../../imgs/eventPage/location.svg";
+import { CreateEventProps } from "../../types";
 
-export const FormFieldLocation = () => {
-  const { control } = useFormContext();
+type FormFieldLocationProps = {
+  label: Path<CreateEventProps>;
+  register: UseFormRegister<CreateEventProps>;
+  required: boolean;
+  errors: FieldErrors<CreateEventProps>;
+};
 
-  const {
-    field: { onChange, value },
-    fieldState: { error },
-  } = useController({
-    name: "location",
-    control,
-    rules: {
-      required: "This field is required",
-    },
-  });
+export const FormFieldLocation = ({
+  label,
+  register,
+  required,
+  errors,
+}: FormFieldLocationProps) => {
+  // const handleRetrieve = (e: any) => {
+  //   const feature = e.features?.[0];
+  //   if (!feature) return;
 
-  const handleRetrieve = (e: any) => {
-    const feature = e.features?.[0];
-    if (!feature) return;
+  //   const address = feature.place_name;
+  //   const [lng, lat] = feature.geometry.coordinates;
 
-    const address = feature.place_name;
-    const [lng, lat] = feature.geometry.coordinates;
-
-    onChange({
-      address,
-      coordinates: new GeoPoint(lat, lng),
-    });
-  };
+  //   onChange({
+  //     address,
+  //     coordinates: new GeoPoint(lat, lng),
+  //   });
+  // };
 
   return (
-    <div className='profile-field'>
-      <div className='profile-field__img'>
+    <div className="profile-field">
+      <div className="profile-field__img">
         <img
-          className='profile-field__icon'
+          className="profile-field__icon"
           src={locationIcon}
-          alt='Location Icon'
+          alt="Location Icon"
         />
       </div>
-      <div className='profile-field__content'>
-        <h4 className='profile-field__label'>Location</h4>
-        <div className='profile-field__input-container'>
-          <AddressAutofill
-            accessToken='pk.eyJ1IjoiYWdpbmVyIiwiYSI6ImNtYm5mcjVuZTFnb3YyanBqaTZkcTUwNW0ifQ.8Xfzx_-MoX4V_uvM_Hhtkw'
-            onRetrieve={handleRetrieve}
-          >
+      <div className="profile-field__content">
+        <h4 className="profile-field__label">Location</h4>
+        <div className="profile-field__input-container">
+          <AddressAutofill accessToken="pk.eyJ1IjoiYWdpbmVyIiwiYSI6ImNtYm5mcjVuZTFnb3YyanBqaTZkcTUwNW0ifQ.8Xfzx_-MoX4V_uvM_Hhtkw">
             <input
-              id='location'
-              type='text'
-              placeholder='Put the address of the event'
-              autoComplete='shipping address-line1'
-              value={value?.address || ""}
-              onChange={(e) => onChange({ ...value, address: e.target.value })}
-              className={`input ${error ? "input--error" : ""}`}
+              id="location"
+              type="text"
+              placeholder="Put the address of the event"
+              autoComplete="shipping address-line1"
+              className={`input ${errors ? "input--error" : ""}`}
+              {...register(label, { required })}
             />
           </AddressAutofill>
 
-          {error && (
-            <div className='input--help-text__container'>
-              <p className='input--help-text input--help-text__error'>
-                {error.message}
+          {errors?.[label] && (
+            <div className="input--help-text__container">
+              <p className="input--help-text input--help-text__error">
+                This field is required
               </p>
             </div>
           )}
--- src/pages/createEvent/CreateEvent.tsx
+++ src/pages/createEvent/CreateEvent.tsx
@@ -13,17 +13,18 @@ import {
 } from "../../functions/Functions";
 import { Button } from "../../components/button/Button";
 import { FormField } from "../../components/formField/FormField";
-import { FormFieldLocation } from "../../components/formField/FormFieldLocation";
-import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { createEventDb } from "../../dataBase/services/createFunctions";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../hooks/auth/AuthContext";
 import imageCompression from "browser-image-compression";
+import { FormFieldLocation } from "../../components/formField/FormFieldLocation";
 import title from "../../imgs/eventPage/title.svg";
 import calendar from "../../imgs/eventPage/calendar.svg";
 import time from "../../imgs/eventPage/time.svg";
+//import location from "../../imgs/eventPage/location.svg";
 import tag from "../../imgs/eventPage/tag.svg";
 import allowedBreeds from "../../imgs/eventPage/dog-side.svg";
 import availability from "../../imgs/eventPage/availability.svg";
@@ -35,13 +36,13 @@ export const CreateEvent = () => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { loggedProfile } = useContext(AuthContext);
-  const methods = useForm<CreateEventProps>();
   const {
     control,
     handleSubmit,
     setError,
+    register,
     formState: { errors },
-  } = methods;
+  } = useForm<CreateEventProps>();
   const navigate = useNavigate();
 
   const handleImageFile = async (file: File) => {
@@ -129,10 +130,7 @@ export const CreateEvent = () => {
         eventPhoto: imageUrl,
         eventDescription: formData.eventDescription,
         dateTime: validDate,
-        location: {
-          address: formData.location.address,
-          coordinates: formData.location.coordinates,
-        },
+        location: formData.location,
         places: Number(formData.places),
         size: formData.size,
         activity: formData.activity,
@@ -158,204 +156,204 @@ export const CreateEvent = () => {
   };
 
   return (
-    <div className='create-event'>
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className='create-event__image-section'>
-            <div className='create-event__image-wrapper'>
-              {!selectedImage && (
-                <div className='create-event__upload-instructions'>
-                  <p>{"Upload an image of the event"}</p>
-                  <label
-                    htmlFor='file-input'
-                    className='create-profile__upload-button'
-                  >
-                    Choose a file
-                  </label>
-                </div>
-              )}
-              <input
-                id='file-input'
-                type='file'
-                accept='image/webp,image/jpeg,image/png'
-                className='create-profile__file-input'
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  if (e.target.files && e.target.files[0]) {
-                    setSelectedImage(e.target.files[0]);
-                  }
-                }}
-                name='eventPhoto'
-              />
-              {selectedImage && (
+    <div className="create-event">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="create-event__image-section">
+          <div className="create-event__image-wrapper">
+            {!selectedImage && (
+              <div className="create-event__upload-instructions">
+                <p>{"Upload an image of the event"}</p>
                 <label
-                  htmlFor='file-input'
-                  className='create-profile__image-label'
+                  htmlFor="file-input"
+                  className="create-profile__upload-button"
                 >
-                  <img
-                    src={URL.createObjectURL(selectedImage)}
-                    alt='Preview'
-                    className='create-event__image-preview'
-                  />
+                  Choose a file
                 </label>
-              )}
-            </div>
+              </div>
+            )}
+            <input
+              id="file-input"
+              type="file"
+              accept="image/webp,image/jpeg,image/png"
+              className="create-profile__file-input"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                if (e.target.files && e.target.files[0]) {
+                  setSelectedImage(e.target.files[0]);
+                }
+              }}
+              name="eventPhoto"
+            />
+            {selectedImage && (
+              <label
+                htmlFor="file-input"
+                className="create-profile__image-label"
+              >
+                <img
+                  src={URL.createObjectURL(selectedImage)}
+                  alt="Preview"
+                  className="create-event__image-preview"
+                />
+              </label>
+            )}
           </div>
-          <p className='create-event__error-text'>
-            {errors.eventPhoto && errors.eventPhoto.message}
-          </p>
-          <div className='create-event__form'>
-            <FormField
-              control={control}
-              iconSrc={title}
-              iconAlt='Event title icon'
-              label='Title'
-              placeholder='Title of the event'
-              editable='string'
-              rules={{
-                required: "Title is necessary",
-                pattern: {
-                  value: /^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\s]{2,70}$/,
-                  message: "Only letters (2–70 characters)",
-                },
-              }}
-              errors={errors.eventTitle?.message}
-              charLimit={70}
-              name='eventTitle'
-            />
-            <FormField
-              control={control}
-              iconSrc={calendar}
-              iconAlt='Event day icon'
-              label='Day'
-              placeholder='Day / Month / Year'
-              editable='string'
-              rules={{
-                required: "Day date is necessary",
-                pattern: {
-                  value: /^([0][1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/,
-                  message: "Day / Month / Year",
-                },
-              }}
-              errors={errors.day && errors.day.message}
-              name='day'
-            />
-            <FormField
-              control={control}
-              iconSrc={time}
-              iconAlt='Event start icon'
-              label='Start time'
-              placeholder='Start time'
-              editable='select'
-              selectData={eventTime}
-              rules={{
-                required: "This field is necessary",
-              }}
-              errors={errors.time && errors.time.message}
-              name='time'
-            />
-            <FormFieldLocation />
-            {/* <FormField
+        </div>
+        <p className="create-event__error-text">
+          {errors.eventPhoto && errors.eventPhoto.message}
+        </p>
+        <div className="create-event__form">
+          <FormField
+            control={control}
+            iconSrc={title}
+            iconAlt="Event title icon"
+            label="Title"
+            placeholder="Title of the event"
+            editable="string"
+            rules={{
+              required: "Title is necessary",
+              pattern: {
+                value: /^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\s]{2,70}$/,
+                message: "Only letters (2–70 characters)",
+              },
+            }}
+            errors={errors.eventTitle?.message}
+            charLimit={70}
+            name="eventTitle"
+          />
+          <FormField
+            control={control}
+            iconSrc={calendar}
+            iconAlt="Event day icon"
+            label="Day"
+            placeholder="Day / Month / Year"
+            editable="string"
+            rules={{
+              required: "Day date is necessary",
+              pattern: {
+                value: /^([0][1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/,
+                message: "Day / Month / Year",
+              },
+            }}
+            errors={errors.day && errors.day.message}
+            name="day"
+          />
+          <FormField
+            control={control}
+            iconSrc={time}
+            iconAlt="Event start icon"
+            label="Start time"
+            placeholder="Start time"
+            editable="select"
+            selectData={eventTime}
+            rules={{
+              required: "This field is necessary",
+            }}
+            errors={errors.time && errors.time.message}
+            name="time"
+          />
+          <FormFieldLocation
+            register={register}
+            label="location"
+            required
+            errors={errors}
+          />
+          {/* <FormField
             control={control}
             iconSrc={location}
-            iconAlt='Event location icon'
-            label='Location'
-            placeholder='Put the address of the event'
-            editable='string'
+            iconAlt="Event location icon"
+            label="Location"
+            placeholder="Put the address of the event"
+            editable="string"
             rules={{
               required: "This field is necessary",
             }}
             errors={errors.location && errors.location.message}
-            name='location'
+            name="location"
           /> */}
-
-            <FormField
-              control={control}
-              iconSrc={tag}
-              iconAlt='Event activity icon'
-              label='Type of activity'
-              placeholder='Select the type of activity'
-              editable='select'
-              selectData={typeOfActivity}
-              rules={{
-                required: "This field is necessary",
-              }}
-              errors={errors.activity && errors.activity.message}
-              name='activity'
-            />
-            <FormField
-              control={control}
-              iconSrc={allowedBreeds}
-              iconAlt='Event allowed breeds icon'
-              label='Allowed breeds'
-              placeholder='Select allowed breed'
-              editable='select'
-              selectData={dogBreedsType}
-              rules={{
-                required: "This field is necessary",
-              }}
-              errors={errors.breeds && errors.breeds.message}
-              name='breeds'
-            />
-            <FormField
-              control={control}
-              iconSrc={ruler}
-              iconAlt='Dog size icon'
-              label='Allowed size'
-              placeholder='Select allowed size'
-              editable='select'
-              selectData={dogSizesType}
-              rules={{
-                required: "This field is necessary",
-              }}
-              errors={errors.size && errors.size.message}
-              name='size'
-            />
-            <FormField
-              control={control}
-              iconSrc={availability}
-              iconAlt='Event availability icon'
-              label='Availability'
-              placeholder='Select the maxium places for the event'
-              editable='select'
-              selectData={maximumPlaces}
-              rules={{
-                required: "This field is necessary",
-              }}
-              errors={errors.places && errors.places.message}
-              name='places'
-            />
-            <FormField
-              control={control}
-              iconSrc={description}
-              iconAlt='Event description icon'
-              label='Description'
-              placeholder='Description of the event'
-              editable='string'
-              rules={{
-                required: "Description is necessary",
-                pattern: {
-                  value: /^.{100,}$/,
-                  message: "Write a description of minimum 100 characters",
-                },
-              }}
-              errors={
-                errors.eventDescription && errors.eventDescription.message
-              }
-              name='eventDescription'
-            />
-            <div className='create-event__button-container'>
-              <Button className='primary'>
-                Publish event
-                {isLoading && (
-                  <div className='spinner'>
-                    <div className='spinner__circle'></div>
-                  </div>
-                )}
-              </Button>
-            </div>
+          <FormField
+            control={control}
+            iconSrc={tag}
+            iconAlt="Event activity icon"
+            label="Type of activity"
+            placeholder="Select the type of activity"
+            editable="select"
+            selectData={typeOfActivity}
+            rules={{
+              required: "This field is necessary",
+            }}
+            errors={errors.activity && errors.activity.message}
+            name="activity"
+          />
+          <FormField
+            control={control}
+            iconSrc={allowedBreeds}
+            iconAlt="Event allowed breeds icon"
+            label="Allowed breeds"
+            placeholder="Select allowed breed"
+            editable="select"
+            selectData={dogBreedsType}
+            rules={{
+              required: "This field is necessary",
+            }}
+            errors={errors.breeds && errors.breeds.message}
+            name="breeds"
+          />
+          <FormField
+            control={control}
+            iconSrc={ruler}
+            iconAlt="Dog size icon"
+            label="Allowed size"
+            placeholder="Select allowed size"
+            editable="select"
+            selectData={dogSizesType}
+            rules={{
+              required: "This field is necessary",
+            }}
+            errors={errors.size && errors.size.message}
+            name="size"
+          />
+          <FormField
+            control={control}
+            iconSrc={availability}
+            iconAlt="Event availability icon"
+            label="Availability"
+            placeholder="Select the maxium places for the event"
+            editable="select"
+            selectData={maximumPlaces}
+            rules={{
+              required: "This field is necessary",
+            }}
+            errors={errors.places && errors.places.message}
+            name="places"
+          />
+          <FormField
+            control={control}
+            iconSrc={description}
+            iconAlt="Event description icon"
+            label="Description"
+            placeholder="Description of the event"
+            editable="string"
+            rules={{
+              required: "Description is necessary",
+              pattern: {
+                value: /^.{100,}$/,
+                message: "Write a description of minimum 100 characters",
+              },
+            }}
+            errors={errors.eventDescription && errors.eventDescription.message}
+            name="eventDescription"
+          />
+          <div className="create-event__button-container">
+            <Button className="primary">
+              Publish event
+              {isLoading && (
+                <div className="spinner">
+                  <div className="spinner__circle"></div>
+                </div>
+              )}
+            </Button>
           </div>
-        </form>
-      </FormProvider>
+        </div>
+      </form>
     </div>
   );
 };
